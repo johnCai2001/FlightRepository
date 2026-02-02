7fe582682682337681a4321639975a6f8c45f4c3
johncai <john10201021@gmail.com>
2026-02-03 00:08:07 +0800

    FlightData Dto

--- src/main/java/com/example/dto/FlightStateDto.java
+++ src/main/java/com/example/dto/FlightStateDto.java
@@ -1,5 +1,21 @@
 package com.example.dto;
 
-public class FlightStateDto {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class FlightStateDto {
+	   private double lon;      
+	    private int heading;     
+	    private int altitude;    
+	    private int speed;
+		public FlightStateDto() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+	    
+	    
 }
