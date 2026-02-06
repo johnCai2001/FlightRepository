70e0463e4b10a5dfef34dcc9ab050394128bf53f
johncai <john10201021@gmail.com>
2026-02-06 12:58:25 +0800

    Flights List (version2)

--- src/main/java/com/example/controller/FlightController.java
+++ src/main/java/com/example/controller/FlightController.java
@@ -1,19 +1,39 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.service.FlightService;
 
 @Controller
 public class FlightController {
 
+	@Autowired
+	private FlightService flightService; 
+	
 	@GetMapping("/")
 	public  String Dash() {
 		System.out.println("Login sucess");
 		return "FlightDash";
 	}
+	@GetMapping("/dashboardPage")
+	public String Login() {
+	    return "redirect:/dashboard";
+	}
+
 	@GetMapping("/dashboard")
-	public  String Login() {
-		return "MainDash";
-}
+	public String dashboard(Model model) {
+	  List<String> flightNumbers = flightService.getFlightNumbers();
+	  model.addAttribute("flightNumbers", flightNumbers);
+	  System.out.println("flightNumbers => " + flightNumbers);
+	  return "MainDash";
+	}
+
+
+	         }
 	
-}
+
